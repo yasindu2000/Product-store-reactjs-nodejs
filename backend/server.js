4e23b90f8e0aa66943610ff67e77a3217655dd13
yasindu2000 <yasindudahanayake@gmail.com>
2025-02-22 11:11:51 +0530

    UPDATE server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";  
 import dotenv from 'dotenv'
 import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
 
 dotenv.config();
 
@@ -8,14 +9,33 @@ dotenv.config();
 const app = express();
 
 
-app.get('/',(req, res)=>{
-    res.send("Server is ready");
+app.use(express.json());
+
+
+app.post('/api/products', async (req, res)=>{
+    const product = req.body; //user send the data
+
+    if(!product.name || !product.price || !product.image){
+        return res.status(400).json({success:false, message: "Please provide all fields"});
+    }
+
+    const newProduct = new Product(product)
+
+    try {
+        await newProduct.save();
+        res.status(201).json({success:true, data:newProduct});
+    } catch (error) {
+        console.error("error in create product:", error.message);
+        res.status(500).json({success:false, message:"Server Error"})
+        
+    }
 })
 
-console.log(process.env.MONGO_URI)
+app.delete("/api/products/:id" , async (req, res)=>{
 
-
-
+    const {id} = req.params;
+    
+})
 
 app.listen(5000, ()=>{
     connectDB();
