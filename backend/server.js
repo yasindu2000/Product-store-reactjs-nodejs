23adc7ea9477fb78a638b1671ba2dc7b188be4d7
yasindu2000 <yasindudahanayake@gmail.com>
2025-02-22 08:57:19 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,20 @@
-const express = require('express');
+import express from "express";  
+
+const app = express();
+
+
+app.get('/',(req, res)=>{
+    res.send("Server is ready");
+})
+
+
+
+
+
+
+app.listen(5000, ()=>{
+    console.log("Server Running port 5000:hellow");
+})
+
+
+// OtzsVrk55QMMcfxD
