6233887c72f3f71915ecd4a711d24a304b80201a
yasindu2000 <yasindudahanayake@gmail.com>
2025-02-22 16:28:20 +0530

    update app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,18 @@
-import { Button } from "@chakra-ui/react"
+import { Box,Button } from "@chakra-ui/react"
 import React from "react"
+import { Route, Routes } from "react-router-dom"
 function App() {
 
 
   return (
-    <Button>Hello</Button>
+    <Box minH={"100vh"}>
+      {/* <Navbar/> */}
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/create" element={<CreatePage/>}/>
+      </Routes>
+
+    </Box>
   )
 }
 
