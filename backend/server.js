0443fdac267da85e14fdad046942caff9ef0c074
yasindu2000 <yasindudahanayake@gmail.com>
2025-02-23 00:43:43 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 import express from "express";  
 import dotenv from 'dotenv'
+import path from 'path'
+
 import { connectDB } from "./config/db.js";
 import productRoutes from './routes/product.route.js'
 
@@ -9,9 +11,19 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve()
+
 app.use(express.json());
 
 app.use("/api/products", productRoutes)
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(5000, ()=>{
     connectDB();
--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,25 +1,6 @@
 import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
-import {
-	Box,
-	Button,
-	Heading,
-	HStack,
-	IconButton,
-	Image,
-	Input,
-	Modal,
-	ModalBody,
-	ModalCloseButton,
-	ModalContent,
-	ModalFooter,
-	ModalHeader,
-	ModalOverlay,
-	Text,
-	useColorModeValue,
-	useDisclosure,
-	useToast,
-	VStack,
-} from "@chakra-ui/react";
+import {Box,Button,Heading,HStack,IconButton,Image,Input,Modal,ModalBody,ModalCloseButton,
+    ModalContent,ModalFooter,ModalHeader,ModalOverlay,Text,useColorModeValue,useDisclosure,useToast,VStack,} from "@chakra-ui/react";
 import { useProductStore } from "../store/product";
 import { useState } from "react";
 
