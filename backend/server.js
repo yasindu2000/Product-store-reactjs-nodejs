a2988f77269d72fbb2231b855b16368db79a5f12
yasindu2000 <yasindudahanayake@gmail.com>
2025-02-22 12:04:18 +0530

    models update

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from 'dotenv'
 import { connectDB } from "./config/db.js";
 import Product from "./models/product.model.js";
+import mongoose from "mongoose";
 
 dotenv.config();
 
@@ -10,6 +11,21 @@ const app = express();
 
 
 app.use(express.json());
+
+app.get("/api/products", async (req, res)=>{
+
+try {
+    const products = await Product.find({});
+    res.status(200).json({ success: true, data:products })
+    
+} catch (error) {
+
+    console.log("error in fetching products:", error.message)
+    res.status(500).json({ success: false, message: "server error"});
+    
+}
+
+})
 
 
 app.post('/api/products', async (req, res)=>{
@@ -31,15 +47,45 @@ app.post('/api/products', async (req, res)=>{
     }
 })
 
+app.put("/api/products/:id", async (req, res)=>{
+
+    const {id} = req.params;
+
+    const product= req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({success:false, message:"Invalid Product Id"});
+    }
+
+    try {
+      const updatedProduct =   await Product.findByIdAndUpdate(id, product,{new:true});
+      res.status(200).json({success:true , message: updatedProduct});
+    } catch (error) {
+        res.status(500).json({success:false, message:"Server Error"});
+    }
+})
+
 app.delete("/api/products/:id" , async (req, res)=>{
 
     const {id} = req.params;
+
+    try {
+        
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({success:true, message:"Product deleted"});
+
+        
+    } catch (error) {
+        console.log("error in deleting product:", error.message)
+        res.status(404).json({success:false, message:"Product not found"});
+        
+    }
     
 })
 
 app.listen(5000, ()=>{
     connectDB();
-    console.log("Server Running port 5000:hellow");
+    console.log("Server Running port 5000 ");
 })
 
 
