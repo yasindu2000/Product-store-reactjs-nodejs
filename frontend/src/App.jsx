56ad86d201318db3ad7917b5afef155af7834901
yasindu2000 <yasindudahanayake@gmail.com>
2025-02-22 14:59:10 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,10 @@
 import { Button } from "@chakra-ui/react"
-
+import React from "react"
 function App() {
 
 
   return (
-    <>
-     <Button>Hello</Button>
-    </>
+    <Button>Hello</Button>
   )
 }
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { ChakraProvider} from '@chakra-ui/react'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ChakraProvider>
-    <App />
+      <App />
     </ChakraProvider>
   </StrictMode>,
 )
